81b2cd0b72ca524648ab5dbc2844071fcb112b3d
kriegsheim <mail@kriegsheim.com>
2024-09-12 15:28:05 +0200

    Image CDN Update

--- src/js/picture.js
+++ src/js/picture.js
@@ -2,10 +2,10 @@
 
 module.exports = (ImageName, ImageAlt) => {
   return `<picture>
-            <source srcset="/assets/img/${ImageName}&width=1920" media="(min-width: 1025px)">
-            <source srcset="/assets/img/${ImageName}&width=1024" media="(min-width: 769px)">
-            <source srcset="/assets/img/${ImageName}&width=768" media="(min-width: 481px)">
+            <source srcset=".netlify/images?url=/assets/img/${ImageName}&width=1920" media="(min-width: 1025px)">
+            <source srcset=".netlify/images?url=/assets/img/${ImageName}&width=1024" media="(min-width: 769px)">
+            <source srcset=".netlify/images?url=/assets/img/${ImageName}&width=768" media="(min-width: 481px)">
 
-            <img src="/assets/img/${ImageName}&width=480" alt="${ImageAlt}" />
+            <img src=".netlify/images?url=/assets/img/${ImageName}&width=480" alt="${ImageAlt}" />
           </picture>`;
 };
