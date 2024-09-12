ff7bbcf4690e70392e4c16aaf12c7bc82ac0d327
kriegsheim <mail@kriegsheim.com>
2024-09-12 15:14:52 +0200

    Update Image CDN

--- src/js/picture.js
+++ src/js/picture.js
@@ -2,10 +2,10 @@
 
 module.exports = (ImageName, ImageAlt) => {
   return `<picture>
-            <source srcset="/assets/img/${ImageName}?nf_resize=fit&w=1920" media="(min-width: 1025px)">
-            <source srcset="/assets/img/${ImageName}?nf_resize=fit&w=1024" media="(min-width: 769px)">
-            <source srcset="/assets/img/${ImageName}?nf_resize=fit&w=768" media="(min-width: 481px)">
+            <source srcset="/assets/img/${ImageName}&width=1920" media="(min-width: 1025px)">
+            <source srcset="/assets/img/${ImageName}&width=1024" media="(min-width: 769px)">
+            <source srcset="/assets/img/${ImageName}&width=768" media="(min-width: 481px)">
 
-            <img src="/assets/img/${ImageName}?nf_resize=fit&w=480" alt="${ImageAlt}" />
+            <img src="/assets/img/${ImageName}&width=480" alt="${ImageAlt}" />
           </picture>`;
 };
